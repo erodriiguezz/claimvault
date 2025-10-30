20200be88a7728eb04adb032cf43200aa3482199
Ernesto Rodriguez <erodriiguezz@icloud.com>
2025-10-29 20:56:15 -0400

    added sections to main page

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="bg-zinc-50 font-sans dark:bg-black">
       <main className="">
+        {/* emergency banner */}
         <div className="bg-red-500 text-white p-4 flex justify-between items-center gap-2">
           <div className="flex items-center gap-2">
             <div className="h-full bg-white/20 inline-block rounded-full p-2">
@@ -18,6 +19,15 @@ export default function Home() {
 
           <button className="bg-white rounded-full text-red-500 px-4 py-1 text-sm font-medium">Call Now</button>
         </div>
+
+        {/* quick actions */}
+        <div></div>
+
+        {/* guides */}
+        <div></div>
+
+        {/* menu */}
+        <div></div>
       </main>
     </div>
   );
